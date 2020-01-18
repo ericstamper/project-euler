23335dfd7d8e1b3c995f4d85b560648befee85ee
Eric Stamper <ericstamper@gmail.com>
2020-01-17 21:10:38 -0500

    Added Problem 005

---
+++ php/problem-005.php
@@ -0,0 +1,35 @@
+<?php
+// Problem #005:
+// 2520 is the smallest number that can be divided by each of the numbers
+// from 1 to 10 without any remainder.
+// What is the smallest positive number that is evenly divisible by all of
+// the numbers from 1 to 20?
+
+// Answer: 232792560
+
+// Solution:
+function smallestMultiple($n)
+{
+    $foundMultiple = false;
+    $currentNumber = $n;
+
+    while ($foundMultiple === false) {
+        $currentNumber++;
+        for ($i = 1; $i <= $n; $i++) {
+            if ($currentNumber % $i === 0) {
+                if ($i === $n) {
+                    $foundMultiple = true;
+                    break;
+                }
+                continue;
+            } else {
+                break;
+            }
+        }
+    }
+
+    return $currentNumber;
+}
+
+// Test:
+echo smallestMultiple(20);
